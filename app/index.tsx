25ac36c9f5249d4c6a6d3dbe4b25b73c2e14467f
Keshia Poedjiono <keshiapud@gmail.com>
2025-09-01 17:57:56 +0800

    Create Sign Up screen UI

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,14 @@
 import { useRouter } from 'expo-router';
-import { Text, View, Button } from 'react-native'
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Pressable, TextInput, Alert } from 'react-native';
+import { SafeAreaView } from 'react-native';
 
 const Home = () => {
     const router = useRouter();
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     const testAPI = async () => {
         const response = await fetch('/api/users', {
             method: 'POST',
@@ -20,11 +26,119 @@ const Home = () => {
     };
 
     return (
-        <View className='flex-1 justify-center items-center'>
-            <Text className='text-red-500'>Home</Text>
-            <Button title="Test API" onPress={() => router.push('/scan')} />
+    <SafeAreaView style={styles.container}>
+        <Text style={styles.greeting}>Welcome!</Text>
+        <Text style={styles.formTitle}>Create your account</Text>
+        <View style={styles.inputContainer}>
+            <Text style={styles.inputTitle}>Name</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Enter your name"
+                placeholderTextColor={'#6A7E97'}
+                value={username}
+                onChangeText={setUsername}
+            />
+            <Text style={styles.inputTitle}>Email Address</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Enter Email"
+                placeholderTextColor={'#6A7E97'}
+                value={email}
+                onChangeText={setEmail}
+            />
+            <Text style={styles.inputTitle}>Password</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Enter Password"
+                placeholderTextColor={'#6A7E97'}
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry={true}
+            />
+            <View>
+                <Pressable
+                style={({ pressed }) => [
+                    styles.button,
+                    {
+                    backgroundColor : 'white',
+                    },
+                ]}>
+                <Text style={styles.buttonText}>Sign Up</Text>
+                </Pressable>
+            </View>
+            <Text style={{marginTop: 10, color: 'white'}}>
+                Already having an account? {' '}
+                <Text
+                    onPress={() => router.replace('')}
+                    style={{ color: 'white', textDecorationLine: 'underline', fontWeight:'bold' }}
+                >
+                Log In
+                </Text>
+            </Text>
         </View>
+    </SafeAreaView>
     )
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'flex-start',
+        backgroundColor: '#bf7641',
+        padding: '3%',
+    },
+    greeting: {
+        paddingHorizontal: '7%',
+        marginTop: 60,
+        fontSize: 35,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color:  'white',
+    },
+    formTitle: {
+        paddingHorizontal: '7%',
+        marginTop: 10,
+        fontSize: 25,
+        textAlign: 'center',
+        color:  'white',
+    },
+    inputContainer: {
+        width: '100%',
+        alignItems: 'center', 
+        paddingHorizontal: '7%',
+        marginTop: 40,
+        
+    }, 
+    inputTitle: {
+        alignSelf: 'flex-start',
+        color:  'white',
+        fontWeight: 'bold'
+    },
+    input: {
+        justifyContent: 'center',
+        width: '100%',
+        height: 50,
+        padding: '3%',
+        margin: 5,
+        fontSize: 17,
+        borderWidth: 1,           
+        borderColor: '#bf7641',     
+        borderRadius: 8,
+        marginBottom: 20,
+        backgroundColor: 'white',
+    },
+    button: {
+        marginTop: 40,
+        paddingVertical: 12,
+        paddingHorizontal: 20,
+        borderRadius: 8,
+        marginVertical: 8,
+        alignItems: 'center',
+    },
+    buttonText: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        color: 'white',
+    },
+})
 export default Home;
