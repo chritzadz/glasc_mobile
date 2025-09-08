4304ba57f580a9e9088eba233d3a0f25f0aee784
chritzadz <chrisdumanauw@gmail.com>
2025-09-08 16:34:06 +0800

    set up GET route for existing personal details

--- app/api/personalDetails+api.ts
+++ app/api/personalDetails+api.ts
@@ -16,3 +16,25 @@ export async function POST(request: Request) {
         return Response.json({ error: (error as Error).message }, { status: 500 });
     }
 }
+
+export async function GET(request: Request) {
+    try {
+        const url = new URL(request.url);
+        const paths = url.pathname.split('/');
+        const userId = paths[paths.length - 1];
+
+        const response = await fetch(`https://glasc-api.netlify.app/api/personalDetails/${userId}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        
+        const data = await response.json();
+        console.log(data);
+        
+        return Response.json(data);
+    } catch (error) {
+        return Response.json({ error: (error as Error).message }, { status: 500 });
+    }
+}
