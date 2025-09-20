601cc7de794c7c5e3e081ce9c5d5edf10a29832b
Keshia Poedjiono <keshiapud@gmail.com>
2025-09-21 06:59:13 +0800

    add search screen header and route

--- app/skincare_routine_search/index.tsx
+++ app/skincare_routine_search/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'expo-router';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView, TouchableOpacity } from 'react-native';
 import { SearchIcon, ChevronLeft } from 'lucide-react-native';
 import { StyleSheet } from 'react-native';
 import React, { useState } from 'react';
@@ -11,14 +11,18 @@ export default function SkincareRoutine() {
     const router = useRouter();
     const [error, setError] = useState<string | null>(null);
 
-    const handleClose = () => {
-        router.push('settings');
+    const handleBack = () => {
+        router.push('skincare_routine_search');
     };
 
     return (
-        <View style={styles.container}>
-            
-        </View>
+        <ScrollView style={styles.container}>
+            <View style={styles.backHeader}>
+                <TouchableOpacity onPress={handleBack}>
+                    <ChevronLeft color="white" />
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
     );
 };
 
@@ -31,5 +35,15 @@ const styles = StyleSheet.create({
         backgroundColor: '#B87C4C',
         paddingTop: 56, // 56px
         paddingBottom: 80, // 80px
+    },
+    backHeader: {
+        display: 'flex',
+        flexDirection: 'row',
+        paddingLeft: 20, // 20px
+        paddingRight: 20, // 20px
+        alignItems: 'center',
+        gap: 5,
+        marginBottom: 10,
+        // "flex flex-row items-center gap-5 mb-10"
     },
 });
