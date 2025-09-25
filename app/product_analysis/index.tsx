b7cea3d31f6e7557dd8fb959a9cc15b6cbf99d50
chritzadz <chrisdumanauw@gmail.com>
2025-09-25 20:37:13 +0800

    fix safe area background of personal skincare_routine

--- app/product_analysis/index.tsx
+++ app/product_analysis/index.tsx
@@ -41,74 +41,77 @@ export default function ProductAnalysisPage() {
     }, []);
 
     return (
-        <SafeAreaView className="flex-1 bg-[#F7F4EA] px-5">
-            <ScrollView className="flex-1">
-                <View className="flex-col flex gap-4">
-                    <View className="flex flex-row justify-center items-center p-2">
-                        <ChevronLeft onPress={() => {router.back();}}></ChevronLeft>
-                        <Text className="font-bold text-2xl flex w-full justify-center items-center">
-                            {productName}
-                        </Text>
-                    </View>
-                    
-                    <View className="flex-row flex-wrap flex items-center mb-2 gap-2">
-                        <View className="flex-row items-center bg-[#5078a7] border-[#B87C4C] border-2 px-2 py-1 rounded-full gap-1">
-                            <Lightbulb size={14} color={"white"}/>
-                            <Text className="text-white font-bold text-sm ">{analysis.type}</Text>
+        <View className="bg-[#F7F4EA] flex-1">
+            <SafeAreaView className="flex-1 px-5">
+                <ScrollView className="flex-1">
+                    <View className="flex-col flex gap-4">
+                        <View className="flex flex-row justify-center items-center p-2">
+                            <ChevronLeft onPress={() => {router.back();}}></ChevronLeft>
+                            <Text className="font-bold text-2xl flex w-full justify-center items-center">
+                                {productName}
+                            </Text>
                         </View>
                         
-                        <View className="flex-row items-center bg-green-600 border-[#B87C4C] border-2 px-2 py-1 rounded-full gap-1">
-                            <Text className="text-white font-bold text-sm">{analysis.match_percentage}%</Text>
-                            <Text className="text-white text-sm font-bold">Match</Text>
-                        </View>
-                    </View>
-
-                    {analysis.harmful_ingredients && analysis.harmful_ingredients.length > 0 ? (
-                        analysis.harmful_ingredients.map((item, idx) => (
-                        <View
-                            key={idx}
-                            className="bg-[#ffe5e5] rounded-lg flex flex-row gap-3 items-center p-2"
-                        >
-                            <AlertTriangle></AlertTriangle>
-                            <View>
-                                <Text className="font-bold">{item.ingredient}</Text>
-                                <Text className="text-sm">{item.why}</Text>
+                        <View className="flex-row flex-wrap flex items-center mb-2 gap-2">
+                            <View className="flex-row items-center bg-[#5078a7] border-[#B87C4C] border-2 px-2 py-1 rounded-full gap-1">
+                                <Lightbulb size={14} color={"white"}/>
+                                <Text className="text-white font-bold text-sm ">{analysis.type}</Text>
+                            </View>
+                            
+                            <View className="flex-row items-center bg-green-600 border-[#B87C4C] border-2 px-2 py-1 rounded-full gap-1">
+                                <Text className="text-white font-bold text-sm">{analysis.match_percentage}%</Text>
+                                <Text className="text-white text-sm font-bold">Match</Text>
                             </View>
                         </View>
-                        ))
-                    ) : (
-                        <Text style={{ marginTop: 8 }}>None detected.</Text>
-                    )}
 
-                    <View className="w-full">
-                        <Text className="font-bold text-2xl flex w-full justify-center items-center">
-                            {"Similar Products"}
-                        </Text>
-                    </View>
-
-                    {Array.from({ length: Math.ceil(skincareProducts.length / 2) }).map((_, rowIdx) => (
-                        <View key={rowIdx} className="flex flex-row gap-2 mb-2">
-                        <View className="flex-1">
-                            <ProductItemBox
-                                imageUrl={'https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=200&q=80'}
-                                name={skincareProducts[rowIdx * 2]?.product_name}
-                                description=''
-                            />
-                        </View>
-                        {skincareProducts[rowIdx * 2 + 1] && (
-                            <View className="flex-1">
-                            <ProductItemBox
-                                imageUrl={'https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=200&q=80'}
-                                name={skincareProducts[rowIdx * 2 + 1]?.product_name}
-                                description=''
-                            />
+                        {analysis.harmful_ingredients && analysis.harmful_ingredients.length > 0 ? (
+                            analysis.harmful_ingredients.map((item, idx) => (
+                            <View
+                                key={idx}
+                                className="bg-[#ffe5e5] rounded-lg flex flex-row gap-3 items-center p-2"
+                            >
+                                <AlertTriangle></AlertTriangle>
+                                <View>
+                                    <Text className="font-bold">{item.ingredient}</Text>
+                                    <Text className="text-sm">{item.why}</Text>
+                                </View>
                             </View>
+                            ))
+                        ) : (
+                            <Text style={{ marginTop: 8 }}>None detected.</Text>
                         )}
+
+                        <View className="w-full">
+                            <Text className="font-bold text-2xl flex w-full justify-center items-center">
+                                {"Similar Products"}
+                            </Text>
                         </View>
-                    ))}
-                </View>
-            </ScrollView>
-        </SafeAreaView>
+
+                        {Array.from({ length: Math.ceil(skincareProducts.length / 2) }).map((_, rowIdx) => (
+                            <View key={rowIdx} className="flex flex-row gap-2 mb-2">
+                            <View className="flex-1">
+                                <ProductItemBox
+                                    imageUrl={'https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=200&q=80'}
+                                    name={skincareProducts[rowIdx * 2]?.product_name}
+                                    description=''
+                                />
+                            </View>
+                            {skincareProducts[rowIdx * 2 + 1] && (
+                                <View className="flex-1">
+                                <ProductItemBox
+                                    imageUrl={'https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=200&q=80'}
+                                    name={skincareProducts[rowIdx * 2 + 1]?.product_name}
+                                    description=''
+                                />
+                                </View>
+                            )}
+                            </View>
+                        ))}
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+        </View>
+        
         
     );
 }
